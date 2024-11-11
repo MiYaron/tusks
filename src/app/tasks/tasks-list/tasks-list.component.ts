36988aa393184234e3ee2f8aeb0aabc9662e6048
yaron.misha <yaronmi92@gmail.com>
2024-11-11 11:59:10 +0200

    added init function

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,8 +1,10 @@
-import { Component, inject } from '@angular/core';
-import { TaskService } from '../task.service';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TaskItemComponent } from './task-item/task-item.component';
+import { Observable } from 'rxjs';
+import { Task } from '../task.model';
+import { TaskService } from '../task.service';
 import { GroupTasksPipe } from '../group-tasks.pipe';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @Component({
   selector: 'app-tasks-list',
@@ -11,7 +13,15 @@ import { GroupTasksPipe } from '../group-tasks.pipe';
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css',
 })
-export class TasksListComponent{
+export class TasksListComponent implements OnInit{
   private taskService = inject(TaskService);
-  public tasks$ = this.taskService.getTasks();
+  public tasks$!: Observable<Task[]>;
+
+  public ngOnInit(): void {
+    this.initFields();
+  }
+
+  private initFields() : void {
+    this.tasks$ = this.taskService.getTasks();
+  }
 }
