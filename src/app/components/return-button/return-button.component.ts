a45b51972978d9b39fcaa5877241c90be7db27c0
MiYaron <yaronmi92@gmail.com>
2024-11-12 13:14:19 +0200

    detached from detecting changes

--- src/app/components/return-button/return-button.component.ts
+++ src/app/components/return-button/return-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Path } from '../../app.paths';
 
@@ -9,8 +9,13 @@ import { Path } from '../../app.paths';
   templateUrl: './return-button.component.html',
   styleUrl: './return-button.component.css'
 })
-export class ReturnButtonComponent {
+export class ReturnButtonComponent implements OnInit{
   private router = inject(Router);
+  private detectorRef = inject(ChangeDetectorRef);
+
+  public ngOnInit(): void {
+    this.detectorRef.detach();
+  }
 
   public goHome(): void {
     this.router.navigate([Path.HOME]);
