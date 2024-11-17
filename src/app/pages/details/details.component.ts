05777e900121a6472bb30cd937bae3d45c17d0e8
MiYaron <yaronmi92@gmail.com>
2024-11-17 19:54:06 +0200

    added error message for invalid form

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -31,7 +31,8 @@ export class DetailsComponent implements OnInit{
 
   private action!: 'add' | 'edit';
   public task!: FormGroup;
-
+  public error!: WritableSignal<string>;
+  
   public ngOnInit(): void {
     this.initFields();
   }
@@ -39,12 +40,15 @@ export class DetailsComponent implements OnInit{
   public saveTask(event: Event): void {
     event.preventDefault();
 
-    this.store.dispatch((TaskActions[this.action]({task: this.task.value})));
-    
-    this.router.navigate([Path.HOME]);
+    if (this.validationCheck()) {
+      this.store.dispatch((TaskActions[this.action]({task: this.task.value})));
+      this.router.navigate([Path.HOME]);
+    }
   }
 
   private initFields(): void {
+    this.error = signal('');
+    
     this.activatedRoute.paramMap.pipe(
       takeUntilDestroyed(this.destroyRef),
       switchMap(params => {
@@ -59,11 +63,29 @@ export class DetailsComponent implements OnInit{
 
   private generateForm(task: Task | undefined) : FormGroup {
     return new FormGroup ({
-      id: new FormControl<string>(task?.id || uuid()),
-      title: new FormControl<string>(task?.title || '', [Validators.required]),
-      desc: new FormControl<string>(task?.desc || ''),
-      deadline: new FormControl<string>(task?.deadline || '', [Validators.required]),
-      isDone: new FormControl<boolean>(task?.isDone || false),
+      id: new FormControl<string>(task?.id ?? uuid()),
+      title: new FormControl<string>(task?.title ?? '', [Validators.required]),
+      desc: new FormControl<string>(task?.desc ?? ''),
+      deadline: new FormControl<string>(task?.deadline ?? '', [Validators.required]),
+      isDone: new FormControl<boolean>(task?.isDone ?? false),
     })
+  }
+
+  private validationCheck () {
+    const title = this.task.get("title");
+    const deadline = this.task.get("deadline");
+
+    if (!title?.valid) {
+      this.error.set('Task must have a title');
+      return false;
+    }
+
+    if (!deadline?.valid) {
+      this.error.set('Task must have a valid date and time');
+      return false;
+    }
+
+    this.error.set ('');
+    return true;
   }
 }
