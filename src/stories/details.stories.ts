68f1e44b23c4efb98fb2948b0dfd68fae44ce7d3
MiYaron <yaronmi92@gmail.com>
2024-11-21 16:00:41 +0200

    added pages

---
+++ src/stories/details.stories.ts
@@ -0,0 +1,25 @@
+import { moduleMetadata, Meta, StoryObj, applicationConfig } from '@storybook/angular';
+import { provideMockStore } from '@ngrx/store/testing';
+import { DetailsComponent } from '../app/pages/details/details.component';
+import { provideRouter } from '@angular/router';
+import { routes } from '../app/app.routes';
+
+const meta: Meta<DetailsComponent> = {
+    title: 'Pages/Details',
+    component: DetailsComponent,
+    decorators: [
+      moduleMetadata({
+        providers: [
+          provideMockStore(),
+        ]
+      }),
+      applicationConfig({
+        providers: [provideRouter(routes)],
+    }),
+    ],
+  };
+
+export default meta;
+type Story = StoryObj<DetailsComponent>;
+
+export const details: Story = {};
---
+++ src/stories/home.stories.ts
@@ -0,0 +1,19 @@
+import { moduleMetadata, Meta, StoryObj } from '@storybook/angular';
+import { provideMockStore } from '@ngrx/store/testing';
+import { HomeComponent } from '../app/pages/home/home.component';
+
+const meta: Meta<HomeComponent> = {
+    title: 'Pages/Home',
+    component: HomeComponent,
+    decorators: [
+      moduleMetadata({
+        providers: [
+          provideMockStore()]
+      }),
+    ],
+  };
+
+export default meta;
+type Story = StoryObj<HomeComponent>;
+
+export const home: Story = {};
